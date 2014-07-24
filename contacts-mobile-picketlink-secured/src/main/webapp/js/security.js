416fb670b9ac5cc8a3a6745da97207b48f6cf9e3
Pedro Igor <pigor.craveiro@gmail.com>
2014-07-23 22:47:51 -0300

    Fixing issues.

--- contacts-mobile-picketlink-secured/src/main/webapp/js/security.js
+++ contacts-mobile-picketlink-secured/src/main/webapp/js/security.js
@@ -38,88 +38,6 @@ $(document).ready(function() {
     var getCurrentTime = CONTACTS.util.getCurrentTime,
         restSecurityEndpoint = CONTACTS.security.restSecurityEndpoint;
 
-    var keycloak = Keycloak('keycloak.json');
-
-    var updateToken = function (callback) {
-        keycloak.updateToken(10)
-            .success(function() {
-                if (keycloak.idToken) {
-                    console.log("[INFO] Updating token.");
-                    securityService.endSession();
-                    securityService.initSession(keycloak.token);
-                    console.log("[INFO] Token updated.");
-
-                    if (callback) {
-                        callback();
-                    }
-                }
-            })
-            .error(function() {
-                console.log("Error from KeyCloak.");
-            });
-    }
-
-    keycloak.init({ onLoad: 'login-required' }).success(updateToken(function() {}));
-
-    /**
-     * Register a handler to be called when Ajax requests complete with an error. Whenever an Ajax request completes
-     * with an error, jQuery triggers the ajaxError event. Any and all handlers that have been registered with the
-     * .ajaxError() method are executed at this time. Note: This handler is not called for cross-domain script and
-     * cross-domain JSONP requests. - from the jQuery docs
-     *
-     * This will be overridden by any ajax call that handles these errors it's self.
-     */
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            securityService.secureRequest(xhr);
-        },
-        error: function( jqXHR, errorThrown ) {
-            if (jqXHR.status == 400) {
-                console.log("[ERROR] Bad request response from the server.");
-            } else if (jqXHR.status == 403) {
-                // Authorization denied. (Does not have permissions)
-                console.log(getCurrentTime() + " [js/security.js] (document.ajaxError) - error in ajax" +
-                    " - jqXHR = " + jqXHR.status +
-                    ", errorThrown = " + errorThrown);
-            } else if (jqXHR.status == 500) {
-                console.log("[ERROR] Internal server error.");
-            } else {
-                console.log("[ERROR] Unexpected error from server.");
-            }
-        }
-    });
-
-    $.ajaxPrefilter(function( opts, originalOptions, jqXHR ) {
-        // you could pass this option in on a "retry" so that it doesn't
-        // get all recursive on you.
-        if ( opts.retryAttempt ) {
-            console.log("Not filtering retry request.");
-            return;
-        }
-
-        var dfd = $.Deferred();
-
-        // if the request works, return normally
-        jqXHR.done(dfd.resolve);
-
-        // if the request fails, do something else
-        // yet still resolve
-        jqXHR.fail(function() {
-            if ( jqXHR.status === 401 ) {
-                originalOptions.retryAttempt = true;
-                updateToken(function() {
-                    console.log("[INFO] Retrying previous request.");
-                    $.ajax(originalOptions).then(dfd.resolve, dfd.reject);
-                });
-            } else {
-                dfd.rejectWith( this, arguments );
-            }
-        });
-
-        // NOW override the jqXHR's promise functions with our deferred
-        return dfd.promise(jqXHR);
-    });
-
     // Log out when the 'Log out' button is clicked.
     $(".security-logout-btn").click(function(e) {
         console.log(getCurrentTime() + " [js/security.js] (#security-logout-btn -> click) - start");
@@ -137,9 +55,92 @@ $(document).ready(function() {
     
     //Initialize all the AJAX form events.
     var initSecurity = function () {
+        //Initialize the vars in the beginning so that you will always have access to them.
+        var getCurrentTime = CONTACTS.util.getCurrentTime;
+
         console.log(getCurrentTime() + " [js/security.js] (initSecurity) - start");
-        //Fetches the initial member data
+
         securityService = new SecurityService();
+
+        var keycloak = Keycloak('keycloak.json');
+
+        var renewToken = function (callbackHandler) {
+            keycloak.updateToken(10)
+                .success(function() {
+                    if (keycloak.idToken) {
+                        console.log("[INFO] Updating token.");
+                        securityService.endSession();
+                        securityService.initSession(keycloak.token);
+                        console.log("[INFO] Token updated.");
+                        callbackHandler();
+                    }
+                })
+                .error(function() {
+                    securityService.endSession();
+                });
+        }
+
+        /**
+         * Register a handler to be called when Ajax requests complete with an error. Whenever an Ajax request completes
+         * with an error, jQuery triggers the ajaxError event. Any and all handlers that have been registered with the
+         * .ajaxError() method are executed at this time. Note: This handler is not called for cross-domain script and
+         * cross-domain JSONP requests. - from the jQuery docs
+         *
+         * This will be overridden by any ajax call that handles these errors it's self.
+         */
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                securityService.secureRequest(xhr);
+            },
+            error: function( jqXHR, errorThrown ) {
+                if (jqXHR.status == 400) {
+                    console.log("[ERROR] Bad request response from the server.");
+                } else if (jqXHR.status == 403) {
+                    // Authorization denied. (Does not have permissions)
+                    console.log(getCurrentTime() + " [js/security.js] (document.ajaxError) - error in ajax" +
+                        " - jqXHR = " + jqXHR.status +
+                        ", errorThrown = " + errorThrown);
+                } else if (jqXHR.status == 500) {
+                    console.log("[ERROR] Internal server error.");
+                } else {
+                    console.log("[ERROR] Unexpected error from server.");
+                }
+            }
+        });
+
+        $.ajaxPrefilter(function( opts, originalOptions, jqXHR ) {
+            // you could pass this option in on a "retry" so that it doesn't
+            // get all recursive on you.
+            if ( opts.retryAttempt ) {
+                console.log("Not filtering retry request.");
+                return;
+            }
+
+            var dfd = $.Deferred();
+
+            // if the request works, return normally
+            jqXHR.done(dfd.resolve);
+
+            // if the request fails, do something else
+            // yet still resolve
+            jqXHR.fail(function() {
+                if ( jqXHR.status === 401 ) {
+                    originalOptions.retryAttempt = true;
+                    renewToken(function() {
+                        console.log("Retrying request...");
+                        $.ajax(originalOptions).then(dfd.resolve, dfd.reject);
+                    });
+                } else {
+                    dfd.rejectWith( this, arguments );
+                }
+            });
+
+            // NOW override the jqXHR's promise functions with our deferred
+            return dfd.promise(jqXHR);
+        });
+
+        keycloak.init({ onLoad: 'login-required' }).success(renewToken);
+
         console.log(getCurrentTime() + " [js/security.js] (initSecurity) - end");
     };
 
